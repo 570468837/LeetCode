26125d815c6c02b1f7eb7a523a80bd9369661a6c
FrankYao <570468837@qq.com>
2017-07-07 00:11:33 +0800

    Q31 Next Permutation

---
+++ src/Q31_Q40/Q31.java
@@ -0,0 +1,38 @@
+package Q31_Q40;
+
+import java.util.Arrays;
+
+public class Q31 {
+	public void nextPermutation(int[] nums) {
+		int length = nums.length;
+		int head = -1;
+        for(int i=length-1; i>=1; i--){
+        	if(nums[i-1] < nums[i]){
+        		head = i-1;
+        		break;
+        	}
+        } 
+        if(head == -1){
+        	Arrays.sort(nums);
+        	return;
+        }
+        //swap
+        for(int i=length-1; i>head; i--){
+        	if(nums[i] > nums[head]){
+        		swap(nums, i, head);
+        		break;
+        	}
+        }
+        //ascend after nums[head]
+        Arrays.sort(nums, head+1, length);
+    }
+	
+	
+	
+	void swap(int[] nums, int m, int n){
+		int temp = nums[m];
+		nums[m] = nums[n];
+		nums[n] = temp;
+	}
+	
+}
