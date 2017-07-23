8b139579901dcf4e3532ad0a85c30f8233606cef
FrankYao <570468837@qq.com>
2017-07-24 00:04:10 +0800

    Q34 binary search

---
+++ src/Q31_Q40/Q34.java
@@ -0,0 +1,25 @@
+package Q31_Q40;
+
+public class Q34 {
+	public int[] searchRange(int[] nums, int target) {
+        int lo = 0, hi = nums.length-1, mid = 0;
+        int find = -1, left, right;
+        while(lo <= hi){
+        	mid = (lo + hi) / 2;
+        	if(nums[mid] == target){find = mid; break;}
+        	if(nums[mid] > target)
+        		hi = mid - 1;
+        	else
+        		lo = mid + 1;
+        }
+        
+        if(find == -1)
+        	return new int[]{-1,-1};
+        for(left=mid; left>=lo && nums[left] == target; left--){};
+        left++;
+        for(right=mid; right<=hi && nums[right] == target; right++){};
+        right--;
+        
+		return new int[]{left, right};
+    }
+}
