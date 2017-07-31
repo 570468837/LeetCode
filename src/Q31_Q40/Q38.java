7bab675f22eddf67dfb2bfcf4603c6fba86c8bf2
FrankYao <570468837@qq.com>
2017-08-01 00:44:27 +0800

    Q38: the description is a bit misunderstanding

---
+++ src/Q31_Q40/Q38.java
@@ -0,0 +1,28 @@
+package Q31_Q40;
+
+public class Q38 {
+	public String countAndSay(int n){
+		if(n==1)
+			return new String("1");
+		String res = "";
+		//the max num is 3.
+		int[] nums = new int[4];
+		String s = countAndSay(n-1);
+		//initial prev
+		int prev = -1;
+		for(int i=0; i<s.length(); i++){
+			char c = s.charAt(i);
+			int num_c = Integer.valueOf(String.valueOf(c));
+			nums[num_c] ++;
+			if(num_c != prev && prev != -1){
+				res += (nums[prev] + String.valueOf(prev));
+				nums[prev] = 0;
+			}
+			prev = num_c;
+		}
+		if(s.substring(s.length()-1).equals(String.valueOf(prev))){
+			res += (nums[prev] + String.valueOf(prev));
+		}
+		return res;
+	}
+}
