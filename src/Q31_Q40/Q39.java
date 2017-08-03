de54e963b421d119b4d6e044188cdf7093fe5a00
FrankYao <570468837@qq.com>
2017-08-03 15:32:54 +0800

    Q39 When recursion happens, note: tempList must be  deeply copied.

---
+++ src/Q31_Q40/Q39.java
@@ -0,0 +1,33 @@
+package Q31_Q40;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q39 {
+	 public static void main(String[] args) {
+		int candidates[] = {2,3,6,7};
+		Q39 q = new Q39();
+		q.combinationSum(candidates, 7);
+	}
+	 
+	 public List<List<Integer>> combinationSum(int[] candidates, int target) {
+		 List<List<Integer>> res = new ArrayList<>();
+		 backtrack(res, new ArrayList<Integer>(), candidates, target, 0);
+	     return res;
+	 }
+	 
+	 public void backtrack(List<List<Integer>> res, List<Integer> tempList, int[] candidates, 
+			 int sum, int start){
+		 if(sum < 0)
+			 return;
+		 else if(sum == 0)
+			 res.add(new ArrayList<Integer>(tempList));
+		 else{
+			 for(int i=start ;i< candidates.length; i++){
+				 tempList.add(candidates[i]);
+				 backtrack(res, tempList, candidates, sum-candidates[i], i);
+				 tempList.remove(tempList.size()-1);
+			 }
+		 }
+	 }
+}
